ffc23a5a81a568cdecf2eb908d5bc89866eec0c4
arinadzyuba <ad89245@student.uwb.edu.pl>
2025-11-13 16:52:31 +0100

    dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,13 @@
 rootProject.name = "licencjat"
 
 include(
-        "app",
+    "app",
     "app:application",
-            "app:ports-output",
-            "app:interfaces",
-            "app:ports-input",
-            "app:infrastructure",
-            "app:commons"
+    "app:ports-output",
+    "app:interfaces",
+    "app:ports-input",
+    "app:infrastructure",
+    "app:commons"
 )
 
 include("app")
