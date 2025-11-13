c450f7db35de35ad1e9749989a78f7c11a304ae3
arinadzyuba <ad89245@student.uwb.edu.pl>
2025-11-13 15:31:17 +0100

    initial commit

---
+++ src/main/kotlin/com/licencjat/danceapp/DanceappApplication.kt
@@ -0,0 +1,11 @@
+package com.licencjat.danceapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DanceappApplication
+
+fun main(args: Array<String>) {
+    runApplication<DanceappApplication>(*args)
+}
