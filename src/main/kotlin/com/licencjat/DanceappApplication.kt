62e261eeadafe8b0d62125c2165c60bf83986a9c
arinadzyuba <ad89245@student.uwb.edu.pl>
2026-01-19 00:55:29 +0100

    Dodanie liquibase, pierwszej tabeli w bazie danych

--- src/main/kotlin/com/licencjat/DanceappApplication.kt
+++ src/main/kotlin/com/licencjat/DanceappApplication.kt
@@ -1,9 +1,11 @@
-package com.licencjat.danceapp
+package com.licencjat
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan(basePackages = ["com.licencjat"])
 class DanceappApplication
 
 fun main(args: Array<String>) {
