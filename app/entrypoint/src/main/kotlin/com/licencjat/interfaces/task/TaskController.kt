60446dfc77042bcf2279fced132ac2c13b3d9b70
arinadzyuba <ad89245@student.uwb.edu.pl>
2026-02-09 02:31:16 +0100

    poprawki-powiększenie rozmiaru filmikow wgrywanych

--- app/entrypoint/src/main/kotlin/com/licencjat/interfaces/task/TaskController.kt
+++ app/entrypoint/src/main/kotlin/com/licencjat/interfaces/task/TaskController.kt
@@ -7,6 +7,7 @@ import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,20 +15,21 @@ class TaskController(
     private val taskService: TaskService
 ) {
 
-
     @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun createTask(
         @RequestParam("title") title: String,
         @RequestParam("description") description: String,
-        @RequestParam("deadline") deadline: String,
+        @RequestParam("deadline") deadline: String, //np. "01.01.2026 15:00"
         @RequestParam("choreographerId") choreographerId: Long,
         @RequestParam("file") file: MultipartFile
     ): TaskResponse {
 
+        val formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm")
+
         val command = CreateTaskCommand(
             title = title,
             description = description,
-            deadline = LocalDateTime.parse(deadline),
+            deadline = LocalDateTime.parse(deadline, formatter),
             file = file,
             choreographerId = choreographerId
         )
